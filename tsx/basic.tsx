9426a560d7aecaf3f91a02b5df90ce21faba5baf
Bilal <bilz44437@gmail.com>
2024-07-31 16:04:30 +0500

    add worldmap v1

---
+++ tsx/basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="basic" tilewidth="64" tileheight="64" tilecount="210" columns="10">
+ <image source="../../Users/Bilal A/Downloads/clear-code-projects 5games main Vampire survivor/data/graphics/tilesets/world_tileset.png" width="640" height="1344"/>
+</tileset>
