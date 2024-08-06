d78045c92b8f845ed943955d9d206ce695fef50b
Bilal <bilz44437@gmail.com>
2024-08-06 19:08:33 +0500

    add and label objects

--- tsx/Objects.tsx
+++ tsx/Objects.tsx
@@ -1,2 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="128" tileheight="192" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../images/objects/palm_inverted.png" width="128" height="192"/>
+ </tile>
+ <tile id="1">
+  <image source="../images/objects/pine.png" width="128" height="192"/>
+ </tile>
+ <tile id="2">
+  <image source="../images/objects/sand_rock.png" width="60" height="60"/>
+ </tile>
+ <tile id="3">
+  <image source="../images/objects/small_tree.png" width="64" height="128"/>
+ </tile>
+ <tile id="4">
+  <image source="../images/objects/trunk.png" width="128" height="192"/>
+ </tile>
+ <tile id="5">
+  <image source="../images/objects/broken_2.png" width="64" height="168"/>
+ </tile>
+ <tile id="6">
+  <image source="../images/objects/broken_marble_1.png" width="64" height="168"/>
+ </tile>
+ <tile id="7">
+  <image source="../images/objects/bushy.png" width="128" height="192"/>
+ </tile>
+ <tile id="8">
+  <image source="../images/objects/grass_rock.png" width="60" height="60"/>
+ </tile>
+ <tile id="9">
+  <image source="../images/objects/large_rock.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../images/objects/marble_pillar.png" width="64" height="168"/>
+ </tile>
+ <tile id="11">
+  <image source="../images/objects/palm.png" width="128" height="192"/>
+ </tile>
+</tileset>
