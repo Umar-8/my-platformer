e3848d36565f9639e4777f25c6e6aec5be229d48
Bilal <bilz44437@gmail.com>
2024-08-25 00:58:04 +0500

    add gun class

--- sprites.py
+++ sprites.py
@@ -13,3 +13,14 @@ class CollisionSprite(pygame.sprite.Sprite):
         self.image = surf
         self.rect = self.image.get_frect(topleft=pos)
 
+class Gun(pygame.sprite.Sprite):
+    def init(self, player, groups):
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1, 0)
+
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images', 'weapons', 'gun', 'golden_friend.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
+
